c34ead31b10ba8197c23587f1c5eeab1da407f05
Jacob <ds0293097@gmail.com>
2025-11-03 15:48:12 -0700

    initial commit

---
+++ src/main/java/net/fosterlink/fosterlinkbackend/FosterLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package net.fosterlink.fosterlinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosterLinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FosterLinkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/fosterlink/fosterlinkbackend/FosterLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fosterlink.fosterlinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosterLinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
