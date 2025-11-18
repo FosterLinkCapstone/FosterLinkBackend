ccbf31888885958a33ab39354035c3bd63820e32
notjacoob <ds0293097@gmail.com>
2025-11-17 22:59:31 -0700

    cors fixes

--- src/main/java/net/fosterlink/fosterlinkbackend/config/SecurityConfig.java
+++ src/main/java/net/fosterlink/fosterlinkbackend/config/SecurityConfig.java
@@ -12,14 +12,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -52,11 +49,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)//todo
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-
                             auth
-                            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                             .requestMatchers(publicEndpoints).permitAll()
                             .requestMatchers(privateEndpoints).authenticated()
                             .anyRequest().authenticated()
--- src/main/java/net/fosterlink/fosterlinkbackend/security/JwtAuthFilter.java
+++ src/main/java/net/fosterlink/fosterlinkbackend/security/JwtAuthFilter.java
@@ -26,6 +26,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String jwt = getJwtFromRequest(request);
 
         if (jwt != null && jwtTokenProvider.validateToken(jwt)) {
