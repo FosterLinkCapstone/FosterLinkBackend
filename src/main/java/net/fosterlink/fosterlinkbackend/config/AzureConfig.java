a1d57494a2244359609e99897434b641cd9993e3
notjacoob <ds0293097@gmail.com>
2025-11-18 00:36:18 -0700

    add forwarded header filter 2

--- src/main/java/net/fosterlink/fosterlinkbackend/config/AzureConfig.java
+++ src/main/java/net/fosterlink/fosterlinkbackend/config/AzureConfig.java
@@ -1,8 +1,10 @@
 package net.fosterlink.fosterlinkbackend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
+@Component
 public class AzureConfig {
     @Bean
     ForwardedHeaderFilter forwardedHeaderFilter() {
